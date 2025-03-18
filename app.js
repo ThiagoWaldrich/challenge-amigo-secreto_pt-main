b5b26995da388b3b2d0da9623da2877262d81e2a
ThiagoWaldrich <thiagowlf7@gmail.com>
2025-03-18 10:46:36 -0300

    Function to add friends and display their names added

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome == "") {
+        alert("Por favor insira um nome");
+        return;
+    }
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+
+    function atualizarLista() {
+        let list = document.getElementById("listaAmigos");
+        list.innerHTML = "";
+
+        amigos.forEach(nome => {
+            let li = document.createElement("li")
+            li.textContent = nome;
+            list.appendChild(li)
+        })
+    }
+}
